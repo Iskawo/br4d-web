80be1cd343ea309389eb379af28248b5f9f74d4c
Iskawo <bradwrightdiscord@gmail.com>
2021-03-11 16:00:13 +0000

    update

--- resources/views/comingsoon.blade.php
+++ resources/views/comingsoon.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Iskawo | Coming Soon</title>
+        <title>Coming Soon - Br4d</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
